67e1ed37a1fa0d0114fac14af8a51c58806a25b6
Katie Mummah <radioactivekate@gmail.com>
2021-12-20 19:17:02 -0600

    running trailmap now calls main from scripts/main

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='trailmap',
-      version='0.0.1',
+      version='0.1.0',
       description='Acquisition Pathway Analysis tool for Cyclus',
       long_description=long_description,
       long_description_content_type="text/markdown",
       author='Katie Mummah',
       author_email='katiemummah@gmail.com',
       url='https://github.com/CNERG/trailmap',
-      packages=find_packages()
+      license_files = ('LICENSE.rst',),
+      packages=find_packages(include=['scripts', 'trailmap']),
+      install_requires=['networkx', 'matplotlib', 'scipy'],
+      entry_points={'console_scripts': ['trailmap=scripts.main:main']}
       )
